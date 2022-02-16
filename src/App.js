d4497d9d7d038cae8a15d6c858817387870bfd32
mrantonlarsson <mr.antonlarsson@gmail.com>
2022-02-16 14:33:49 +0100

    template from tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Notes from './pages/Notes'
+import Create from './pages/Create'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { purple, grey } from '@mui/material/colors';
+import ResponsiveAppBar from './components/ResponsiveAppbar';
+
+const theme = createTheme({
+  palette: {
+    primary: grey,
+    secondary: purple
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+      <ResponsiveAppBar>
+        <Switch>
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </ResponsiveAppBar>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/NoteCard.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import { Avatar, IconButton, Typography } from '@mui/material';
+import { DeleteOutlined } from '@mui/icons-material';
+import { makeStyles } from '@mui/styles';
+import { blue, green, pink, yellow } from '@mui/material/colors';
+
+
+
+const useStyles = makeStyles({
+
+})
+
+export default function NoteCard({ note, handleDelete }) {
+    const classes = useStyles(note)
+  return (
+    <div>
+        <Card elevation={1}>
+            <CardHeader 
+                avatar = {
+                    <Avatar 
+                        sx={{backgroundColor: note.category == "work" ? yellow[700] : note.category == "money" ? green[500] : note.category == "todos" ? pink[500] : blue[500]}}  
+                    >
+                        {note.category[0].toUpperCase()}
+                    </Avatar>
+                }
+                action={
+                    <IconButton onClick={() => handleDelete(note.id)}>
+                        <DeleteOutlined />
+                    </IconButton>
+                }
+                title={note.title}
+                subheader={note.category}
+            />
+            <CardContent>
+                <Typography variant="body2" color="textSecondary">
+                    {note.details}
+                </Typography>
+            </CardContent>
+        </Card>
+    </div>
+  )
+}
